1ee034e383e0ba9947c46055e3f2228043fe959f
MatthewFender4 <57487017+MatthewFender4@users.noreply.github.com>
2020-01-05 08:26:16 +0000

    Website with Hue Lights (Reupload)
    REST API for the Hue Lights now working and integrated with the password checking functions, currently configured to work on my personal Hue Lights, not UoP. Version reuploaded due to original folder name being incorrect.

--- SOFT166 Password Checker/ControlAndLights.js
+++ SOFT166 Password Checker/ControlAndLights.js
@@ -4,10 +4,16 @@ const upperCaseStr = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const numeralStr = "0123456789";
 const specialStr = "#?!$£%&_()[],./+-";
 
+//The lamp hues
+const redHue = 65535;
+const yellowHue = 10500;
+const greenHue = 25500;
+
+//Strength of the password and the output comments on what is missing
 var strengthCount;
 var passwordStrengthMessage;
 
-//Just a small example list for use as a proof of concept
+//Just an illustrative sample a full development would use a server based file download with a few thousand names. It would contain common passwords, childrens names and pets names
 const commonNameArray = [
   "Password",
   "password",
@@ -62,16 +68,18 @@ const commonNameArray = [
 ];
 
 function passwordCheck(inputStr) {
-  //Reset the password strength count
+  //Reset the password strength count and the message
   strengthCount = 0;
   passwordStrengthMessage = "";
-  //Overview
-  //get the password string
-  // document.getElementById("demo").textContent = inputStr;
-  //now use it for each of the checks
-  //also do each of the checks with every update!
-  //by using the input 'oninput' as the event trigger, checks will be done if the user adds OR deletes characters
-  // so ensuring the latest value of the password is always checked
+  /*
+   Overview
+   get the password string
+   document.getElementById("demo").textContent = inputStr;
+   now use it for each of the checks
+   also do each of the checks with every update!
+   by using the input 'oninput' as the event trigger, checks will be done if the user deletes characters
+   so ensuring the latest value of the password is always checked
+  */
   doLowerCaseCheck(inputStr);
   doUpperCaseCheck(inputStr);
   doNumeralCheck(inputStr);
@@ -87,12 +95,18 @@ function doLowerCaseCheck(inputStr) {
     if (v > -1) {
       //lower case found
       document.getElementById("lowerCheck").style.backgroundColor = "green";
+      //switch on the hue light 1 to green
+      lightToColour("on", 1, greenHue);
+      //show that has happened to the userusing the indicator button
+      setHueIndicator("L1", "green", "ON Green");
       strengthCount++;
       return;
     }
   }
   //lower case char was not found
   document.getElementById("lowerCheck").style.backgroundColor = "red";
+  lightToColour("on", 1, redHue);
+  setHueIndicator("L1", "red", "ON Red");
   passwordStrengthMessage = "No Lower case char;";
   return;
 }
@@ -103,12 +117,16 @@ function doUpperCaseCheck(inputStr) {
     if (v > -1) {
       //upper case found
       document.getElementById("upperCheck").style.backgroundColor = "green";
+      lightToColour("on", 2, greenHue);
+      setHueIndicator("L2", "green", "ON Green");
       strengthCount++;
       return;
     }
   }
   //upper case char was not found
   document.getElementById("upperCheck").style.backgroundColor = "red";
+  lightToColour("on", 2, redHue);
+  setHueIndicator("L2", "red", "ON Red");
   passwordStrengthMessage += " No Upper Case char;";
   return;
 }
@@ -119,12 +137,16 @@ function doNumeralCheck(inputStr) {
     if (v > -1) {
       //numeral found
       document.getElementById("numberCheck").style.backgroundColor = "green";
+      lightToColour("on", 3, greenHue);
+      setHueIndicator("L3", "green", "ON Green");
       strengthCount++;
       return;
     }
   }
   //numeral was not found
   document.getElementById("numberCheck").style.backgroundColor = "red";
+  lightToColour("on", 3, redHue);
+  setHueIndicator("L3", "red", "ON Red");
   passwordStrengthMessage += " No numeral;";
   return;
 }
@@ -135,31 +157,40 @@ function doSpecialCheck(inputStr) {
     if (v > -1) {
       //special char found
       document.getElementById("specialCheck").style.backgroundColor = "green";
+      lightToColour("on", 4, greenHue);
+      setHueIndicator("L4", "green", "ON Green");
       strengthCount++;
       return;
     }
   }
   //special char was not found
   document.getElementById("specialCheck").style.backgroundColor = "red";
+  lightToColour("on", 4, redHue);
+  setHueIndicator("L4", "red", "ON Red");
   passwordStrengthMessage += " No Special Character;";
   return;
 }
 
 function doLengthCheck(inputStr) {
-  //length check is the only check which isnt binary
+  //length check is the first check which isnt binary
   // it uses red when length is far too short
-  // then yellow when the password is closer to a sufficient length
-  // then green when it meets the required minimum
+  // then yellow when it is getting there and green when it meets the required minimum
 
   if (inputStr.length < 6) {
     document.getElementById("lenCheck").style.backgroundColor = "red";
+    lightToColour("on", 5, redHue);
+    setHueIndicator("L5", "red", "ON Red");
     passwordStrengthMessage += " Far too short;";
   } else if (inputStr.length >= 6 && inputStr.length < 8) {
     document.getElementById("lenCheck").style.backgroundColor = "yellow";
+    lightToColour("on", 5, yellowHue);
+    setHueIndicator("L5", "yellow", "ON Yellow");
     passwordStrengthMessage += " Add a couple more characters;";
     strengthCount++;
   } else {
     document.getElementById("lenCheck").style.backgroundColor = "green";
+    lightToColour("on", 5, greenHue);
+    setHueIndicator("L5", "green", "ON Green");
     strengthCount++;
     strengthCount++;
   }
@@ -170,7 +201,6 @@ function doCommonCheck(inputStr) {
 
   for (i = 0; i < commonNameArray.length; i++) {
     //only check if the name fits!
-
     //check if the common word from the array is anywhere in the input string
     if (inputStr.length >= commonNameArray[i].length) {
       var v = inputStr.indexOf(commonNameArray[i]);
@@ -180,7 +210,7 @@ function doCommonCheck(inputStr) {
         strengthCount--;
         passwordStrengthMessage +=
           "Includes Common word: " + commonNameArray[i];
-        //Common name was found
+        //Common name was found tell user what it was
         found = true;
         break; //break out of the for loop when first common word is found
       }
@@ -208,14 +238,20 @@ function doOverallStrength(inputStr) {
     document.getElementById("passwordStrength").style.backgroundColor = "red";
     strengthStr = "Very Weak: ";
     document.getElementById("strengthHeader").style.backgroundColor = "red";
+    lightToColour("on", 6, redHue);
+    setHueIndicator("L6", "red", "ON Red");
   } else if (strengthCount >= 4 && strengthCount < 7) {
     document.getElementById("passwordStrength").style.backgroundColor =
       "yellow";
     document.getElementById("strengthHeader").style.backgroundColor = "yellow";
+    lightToColour("on", 6, yellowHue);
+    setHueIndicator("L6", "yellow", "ON Yellow");
     strengthStr = "Weak: ";
   } else {
     document.getElementById("passwordStrength").style.backgroundColor = "green";
     document.getElementById("strengthHeader").style.backgroundColor = "green";
+    lightToColour("on", 6, greenHue);
+    setHueIndicator("L6", "green", "ON Green");
     strengthStr = "STRONG: ";
   }
 
@@ -225,4 +261,56 @@ function doOverallStrength(inputStr) {
   document.getElementById(
     "strengthComment"
   ).textContent = passwordStrengthMessage;
+}
+
+/*
+  Hue light management section of code
+  Key elements taken from the lab work and adapted
+*/
+
+function getLightURI(lightNum) {
+  //using home Hue lights for testing
+  var IP = "http://192.168.0.17/api/"; //!!!! Change this back to http://192.168.0.50/api/
+  var username = "xME26iv6t6YwFvx26rRmOkxU4a5MuwZGuXztilX0"; // !!!!!! Change back to "stlaB2I6VZ8O80Qepc-1xfmLrHgyTFvB9IGupaQz" when back in the labs;
+  var lights = "/lights/";
+  var URI = IP + username + lights;
+  return URI + lightNum + "/";
+}
+
+function lightToColour(onStatus, lightNum, colorHue) {
+  // this function will set a numbered light to a requested colour and confirm success
+  // the brightness and saturation values remain the same for all calls
+  var lightState = { on: onStatus, hue: colorHue, sat: 245, bri: 100 };
+
+  $.ajax({
+    url: getLightURI(lightNum) + "state/",
+    type: "PUT",
+    data: JSON.stringify(lightState)
+  });
+}
+
+function activateAllHueLights() {
+  //this checks the link to the hue lights works and sets all 6 lights to red
+  //first switch them all off to ensure we get rid off any other settings
+  for (i = 1; i < 7; i++) {
+    //change this to i once all lights working
+    lightToColour(false, i, redHue); //hue is ignored
+  }
+  //now set them all to on and red
+  for (i = 1; i < 7; i++) {
+    //change this to i once all lights working
+    lightToColour(true, i, redHue);
+    var idStr = "L" + i.toString();
+    setHueIndicator(idStr, "red", "ON Red");
+  }
+   //If resetting the lights delete any password present at that time
+   document.getElementById("passwordInput").value = "";
+ 
+}
+
+function setHueIndicator(idStr, bgColor, btnText) {
+  //Feedback to show hue light status
+  document.getElementById(idStr).style.backgroundColor = bgColor;
+  //Saying what the colour is in case user is colour blind
+  document.getElementById(idStr).textContent = btnText;
 }
